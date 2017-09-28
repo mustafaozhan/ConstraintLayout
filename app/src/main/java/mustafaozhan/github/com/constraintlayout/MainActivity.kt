d3b19f0ddd3d8d2b8473bc80da6f58501b6c2e98
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2017-09-28 16:41:10 +0300

    Match Parent Animation for Image View with Constraint Layout

--- app/src/main/java/mustafaozhan/github/com/constraintlayout/MainActivity.kt
+++ app/src/main/java/mustafaozhan/github/com/constraintlayout/MainActivity.kt
@@ -1,15 +1,69 @@
 package mustafaozhan.github.com.constraintlayout
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
+import android.transition.TransitionManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private var constraintLayout: ConstraintLayout? = null
+    private val applyConstraintSet = ConstraintSet()
+    private val resetConstraintSet = ConstraintSet()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnVote.setOnClickListener { startActivity(Intent(this, VoteActivity::class.java)) }
+
+        constraintLayout = findViewById(R.id.main) as ConstraintLayout
+        resetConstraintSet.clone(constraintLayout)
+        applyConstraintSet.clone(constraintLayout)
+
+
+        var default = true
+        imgWallpaper.setOnClickListener {
+
+            default = if (default) {
+                fullScreenEffect(it.id)
+                !default
+            } else {
+                resetEffect()
+                !default
+            }
+
+        }
+
+    }
+
+    private fun resetEffect() {
+        TransitionManager.beginDelayedTransition(constraintLayout)
+        resetConstraintSet.applyTo(constraintLayout)
+    }
+
+    private fun fullScreenEffect(id: Int) {
+        hideOthers(id)
+        TransitionManager.beginDelayedTransition(constraintLayout)
+        applyConstraintSet.clear(id)
+        applyConstraintSet.connect(id, ConstraintSet.LEFT, R.id.main, ConstraintSet.LEFT, 0)
+        applyConstraintSet.connect(id, ConstraintSet.RIGHT, R.id.main, ConstraintSet.RIGHT, 0)
+        applyConstraintSet.connect(id, ConstraintSet.TOP, R.id.main, ConstraintSet.TOP, 0)
+        applyConstraintSet.connect(id, ConstraintSet.BOTTOM, R.id.main, ConstraintSet.BOTTOM, 0)
+        applyConstraintSet.applyTo(constraintLayout)
+    }
+
+    private fun hideOthers(id: Int) {
+        applyConstraintSet.setVisibility(R.id.btnCancel, ConstraintSet.GONE)
+        applyConstraintSet.setVisibility(R.id.btnVote, ConstraintSet.GONE)
+        applyConstraintSet.setVisibility(R.id.imgWallpaper, ConstraintSet.GONE)
+        applyConstraintSet.setVisibility(R.id.imgLogo, ConstraintSet.GONE)
+        applyConstraintSet.setVisibility(R.id.txtInformation, ConstraintSet.GONE)
+        applyConstraintSet.setVisibility(R.id.txtName, ConstraintSet.GONE)
+        applyConstraintSet.setVisibility(R.id.txtNameResult, ConstraintSet.GONE)
+        applyConstraintSet.setVisibility(R.id.txtRelease, ConstraintSet.GONE)
+        applyConstraintSet.setVisibility(R.id.txtReleaseResult, ConstraintSet.GONE)
+
+        applyConstraintSet.setVisibility(id, ConstraintSet.VISIBLE)
+
+
     }
 }
